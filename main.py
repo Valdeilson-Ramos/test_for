367ea97a2c9d09ba86baa2659b97fc8059d92a1b
Valdeilson Ramos <42836882+Valdeilson-Ramos@users.noreply.github.com>
2021-11-16 14:01:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''' s = 0 
+for x in range(1,20,3):
+  s = s+x
+print('Soma = ', s) 
+
+lista = [3.4, 6.6, 8, 9, 10, 9.5, 8.8, 4.3]
+soma = 0
+for nota in lista:
+  soma = soma+nota
+media = soma/len(lista)
+print('Média = ', media)'''
+
+L = [3 , 'abacate' , 9.7 , [5 , 6 , 3] , "Python" , (3 , 'j')]
